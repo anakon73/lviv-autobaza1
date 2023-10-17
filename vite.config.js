fd32f807a90e185b886670e79845aa88bd8e2b2a
anakon73 <nazarkinah73@gmail.com>
2023-10-18 00:06:31 +0300

    fix(vite.config): remove products input for build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
 
-export default defineConfig ({
+export default defineConfig({
   plugins: [handlebars({
     partialDirectory: resolve(__dirname, 'src/partials'),
   })],
@@ -10,7 +10,6 @@ export default defineConfig ({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        products: resolve(__dirname, '/products/index.html')
       }
     }
   }
