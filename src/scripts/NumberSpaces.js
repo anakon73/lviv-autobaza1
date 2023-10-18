c1c4fcfefd87787837ea49ef8472bbdae294c94e
anakon73 <nazarkinah73@gmail.com>
2023-10-18 13:42:44 +0300

    fix(Header): truncate cart cost

--- src/scripts/NumberSpaces.js
+++ src/scripts/NumberSpaces.js
@@ -2,10 +2,10 @@ function numberWithSpaces(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
-const balance = document.getElementById('balance')
+const cartCost = document.getElementById('cartCost')
 
-function balanceWithSpaces() {
-  balance.innerText = numberWithSpaces(balance.innerText)
+function cartCostWithSpaces() {
+  cartCost.innerText = numberWithSpaces(cartCost.innerText)
 }
 
-balanceWithSpaces()
+cartCostWithSpaces()
