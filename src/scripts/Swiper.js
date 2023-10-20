d7f8e355e1cb5056e33227c976bb167904677ea1
anakon73 <nazarkinah73@gmail.com>
2023-10-20 15:52:48 +0300

    feat(WeakDiscountSwiper): implement

---
+++ src/scripts/Swiper.js
@@ -0,0 +1,17 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 2,
+  spaceBetween: 114,
+  loop: true,
+  centeredSlides: true,
+});
