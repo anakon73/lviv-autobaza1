7cf1c6b381c7934ab7cc4caab25dbbedaa10da3d
anakon73 <nazarkinah73@gmail.com>
2023-10-26 15:48:24 +0300

    feat(ReviewsSwiper): implement

--- src/scripts/ReviewsSwiper.js
+++ src/scripts/ReviewsSwiper.js
@@ -1,6 +1,18 @@
 import Swiper from 'swiper'
 import 'swiper/css'
+import 'swiper/css/pagination'
+import { Navigation, Pagination } from 'swiper/modules'
 
 const reviewsSwiper = new Swiper('.reviewsSwiper', {
-  slidesPerView: 1,
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.nextReview',
+    prevEl: '.prevReview',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 2,
+  spaceBetween: 20,
 })
