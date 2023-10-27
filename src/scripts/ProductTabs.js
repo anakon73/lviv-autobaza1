1fb1cba4a5dae00294c232c55abad81dfd2bf50f
anakon73 <nazarkinah73@gmail.com>
2023-10-27 12:08:18 +0300

    refactor(ProductTabs): optimazed code

--- src/scripts/ProductTabs.js
+++ src/scripts/ProductTabs.js
@@ -1,19 +1,23 @@
-const selectedTabBtnClasses = ['underline', 'decoration-2', 'underline-offset-[17px]', 'font-semibold']
+const selectedTabBtnClasses = [
+  'underline',
+  'decoration-2',
+  'underline-offset-[17px]',
+  'font-semibold',
+]
 
 const tabs = document.querySelectorAll('.tab')
 const tabsBtn = document.querySelectorAll('.tabBtn')
 
 tabsBtn.forEach((btn) => {
-  btn.addEventListener('click', function () {
-    let currentBtn = btn
-    let tabId = currentBtn.getAttribute('data-tab')
-    let currentTab = document.querySelector(tabId)
+  btn.addEventListener('click', () => {
+    const tabId = btn.getAttribute('data-tab')
+    const currentTab = document.querySelector(tabId)
 
     if (currentTab.classList.contains('hidden')) {
       tabsBtn.forEach((item) => item.classList.remove(...selectedTabBtnClasses))
       tabs.forEach((item) => item.classList.add('hidden'))
 
-      currentBtn.classList.add(...selectedTabBtnClasses)
+      btn.classList.add(...selectedTabBtnClasses)
       currentTab.classList.remove('hidden')
       if (currentTab.id === 'specsTab') currentTab.classList.add('grid')
     }
