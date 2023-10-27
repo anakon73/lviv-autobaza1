169eb1bfe14ba20cf4fb5d8a4965139bb93fbef7
anakon73 <nazarkinah73@gmail.com>
2023-10-27 20:16:07 +0300

    feat(Search): implement

---
+++ src/scripts/Search.js
@@ -0,0 +1,36 @@
+const search = document.getElementById('search')
+const searchInput = document.getElementById('searchInput')
+const overlay = document.getElementById('overlay')
+const searchResult = document.getElementById('searchResult')
+
+let searchValue = searchInput.value
+
+searchInput.oninput = () => {
+  searchValue = searchInput.value
+  onOpen()
+}
+
+const onOpen = () => {
+  overlay.classList.remove('hidden')
+  if (searchValue !== '') {
+    searchResult.classList.remove('hidden')
+    searchResult.classList.add('flex')
+  }
+  console.log('open');
+}
+
+const onClose = () => {
+  overlay.classList.add('hidden')
+  searchResult.classList.add('hidden')
+  searchResult.classList.remove('flex')
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    onClose()
+    searchInput.blur()
+  }
+})
+
+search.addEventListener('click', onOpen)
+overlay.addEventListener('click', onClose)
