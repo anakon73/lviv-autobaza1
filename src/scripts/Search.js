e3153ca3fe77b597be2f8f3402cb7f25e15ca817
anakon73 <nazarkinah73@gmail.com>
2023-10-29 14:18:57 +0200

    refactor(Search): implement isOpen variable

--- src/scripts/Search.js
+++ src/scripts/Search.js
@@ -2,27 +2,40 @@ const search = document.getElementById('search')
 const searchInput = document.getElementById('searchInput')
 const overlay = document.getElementById('overlay')
 const searchResult = document.getElementById('searchResult')
+const searchBtn = document.getElementById('searchBtn')
+
+let isOpen = false
 
 let searchValue = searchInput.value
 
 searchInput.oninput = () => {
   searchValue = searchInput.value
-  onOpen()
+
+  if (searchInput.value === '') {
+    onClose()
+    overlay.classList.remove('hidden')
+    searchResult.classList.add('hidden')
+  } else {
+    onOpen()
+  }
 }
 
 const onOpen = () => {
-  overlay.classList.remove('hidden')
-  searchResult.classList.add('hidden')
-  if (searchValue !== '') {
+  isOpen = true
+  if (isOpen && searchValue !== '') {
+    overlay.classList.remove('hidden')
     searchResult.classList.remove('hidden')
     searchResult.classList.add('flex')
   }
 }
 
 const onClose = () => {
-  overlay.classList.add('hidden')
-  searchResult.classList.add('hidden')
-  searchResult.classList.remove('flex')
+  isOpen = false
+  if (!isOpen) {
+    overlay.classList.add('hidden')
+    searchResult.classList.add('hidden')
+    searchResult.classList.remove('flex')
+  }
 }
 
 document.addEventListener('keydown', (e) => {
@@ -32,11 +45,23 @@ document.addEventListener('keydown', (e) => {
   }
 })
 
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (searchValue !== '') {
+    window.location.href = '/results/'
+    return
+  }
+  if (!isOpen) {
+    overlay.classList.remove('hidden')
+    searchResult.classList.add('hidden')
+  }
+})
+
 search.addEventListener('click', onOpen)
 overlay.addEventListener('click', onClose)
 
 document.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') {
-    window.location.href = '/results/';
+  if (e.key === 'Enter' && isOpen && searchValue !== '') {
+    window.location.href = '/results/'
   }
 })
