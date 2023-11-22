07b7e6567b924bfb18d07456a7be607a135c73c9
anakon73 <nazarkinah73@gmail.com>
2023-11-23 01:07:20 +0200

    feat(ReviwesPage): adaptive

--- src/scripts/ReviewModal.js
+++ src/scripts/ReviewModal.js
@@ -1,46 +1,49 @@
 /** @type HTMLDialogElement */
-const modalWindow = document.getElementById('modalWindow')
-const openBtn = document.getElementById('openReviewModal')
-const closeBtn = document.getElementById('closeReviewModal')
-const xMarkClose = document.getElementById('xMarkClose')
-const form = document.getElementById('form')
-const sendBtn = document.getElementById('sendBtn')
-const successfully = document.getElementById('successfully')
+const modalWindow = document.getElementById("modalWindow")
+const openBtn = document.getElementById("openReviewModal")
+const closeBtn = document.getElementById("closeReviewModal")
+const xMarkClose = document.getElementById("xMarkClose")
+const form = document.getElementById("form")
+const sendBtn = document.getElementById("sendBtn")
+const successfully = document.getElementById("successfully")
 
 const onClose = () => {
-  form.classList.remove('hidden')
-  form.classList.add('flex')
+  form.classList.remove("hidden")
+  form.classList.add("flex")
 
-  successfully.classList.add('hidden')
-  successfully.classList.remove('flex')
+  successfully.classList.add("hidden")
+  successfully.classList.remove("flex")
+
+  document.body.classList.remove("overflow-hidden")
 
   modalWindow.close()
 }
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') onClose()
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") onClose()
 })
 
-openBtn.addEventListener('click', () => {
+openBtn.addEventListener("click", () => {
   modalWindow.showModal()
+  document.body.classList.add("overflow-hidden")
 })
 
-closeBtn.addEventListener('click', onClose)
-xMarkClose.addEventListener('click', onClose)
+closeBtn.addEventListener("click", onClose)
+xMarkClose.addEventListener("click", onClose)
 
-modalWindow.addEventListener('click', (e) => {
+modalWindow.addEventListener("click", (e) => {
   if (e.target === modalWindow) onClose()
 })
 
-sendBtn.addEventListener('click', () => {
-  console.log(form);
-  console.log(successfully);
+sendBtn.addEventListener("click", () => {
+  console.log(form)
+  console.log(successfully)
 
-  form.classList.add('hidden')
-  form.classList.remove('flex')
+  form.classList.add("hidden")
+  form.classList.remove("flex")
 
-  successfully.classList.remove('hidden')
-  successfully.classList.add('flex')
+  successfully.classList.remove("hidden")
+  successfully.classList.add("flex")
 })
 
-if (form) form.addEventListener('click', (e) => e.preventDefault())
+if (form) form.addEventListener("click", (e) => e.preventDefault())
