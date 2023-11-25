243caee327f7bf38aa23659a9d11107d4ffe2ff8
anakon73 <nazarkinah73@gmail.com>
2023-11-25 18:21:46 +0200

    feat(ProfilePage): adaptive

--- src/scripts/ProfileTabs.js
+++ src/scripts/ProfileTabs.js
@@ -1,29 +1,50 @@
-const tabs = document.querySelectorAll('.tab')
-const tabsBtn = document.querySelectorAll('.tabBtn')
+const tabs = document.querySelectorAll(".tab")
+const tabsBtn = document.querySelectorAll(".tabBtn")
 
-tabsBtn.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const tabId = btn.getAttribute('data-tab')
+tabsBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const tabId = btn.getAttribute("data-tab")
     const currentTab = document.querySelector(tabId)
 
-    if (currentTab.classList.contains('hidden')) {
-      tabs.forEach(item => item.classList.add('hidden'))
-      tabsBtn.forEach(item => {
-        item.querySelector('p').classList.remove('font-semibold', 'text-white', 'bg-blue-600')
-        item.querySelector('p').classList.add('font-medium', 'text-slate', 'bg-gray-100')
-        btn.querySelector('img').classList.add('bg-gray-100')
-        item.querySelector('img').classList.remove('bg-blue-600')
+    if (currentTab.classList.contains("hidden")) {
+      tabs.forEach((item) => item.classList.add("hidden"))
+      tabsBtn.forEach((item) => {
+        const textParagraph = item.querySelector("p")
+        const image = item.querySelector("img")
+
+        textParagraph.classList.remove(
+          "font-semibold",
+          "text-white",
+          "bg-blue-600",
+        )
+        textParagraph.classList.add("font-medium", "text-slate", "bg-gray-100")
+
+        image.classList.remove("bg-blue-600")
+        image.classList.add("bg-gray-100")
       })
 
-      btn.querySelector('p').classList.remove('font-medium', 'text-slate', 'bg-gray-100')
-      btn.querySelector('img').classList.remove('bg-gray-100')
-      btn.querySelector('p').classList.add('font-semibold', 'text-white', 'bg-blue-600')
-      btn.querySelector('img').classList.add('bg-blue-600')
-      currentTab.classList.remove('hidden')
+      const currentTextParagraph = btn.querySelector("p")
+      const currentImage = btn.querySelector("img")
 
-      if (currentTab.id === 'favoriteTab') currentTab.classList.add('grid')
+      currentTextParagraph.classList.remove(
+        "font-medium",
+        "text-slate",
+        "bg-gray-100",
+      )
+      currentImage.classList.remove("bg-gray-100")
+
+      currentTextParagraph.classList.add(
+        "font-semibold",
+        "text-white",
+        "bg-blue-600",
+      )
+      currentImage.classList.add("bg-blue-600")
+
+      currentTab.classList.remove("hidden")
+
+      if (currentTab.id === "favoriteTab") currentTab.classList.add("grid")
     }
   })
 })
 
-document.querySelector('.tabBtn').click()
+document.querySelector(".tabBtn").click()
