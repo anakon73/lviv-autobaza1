b25ef7bcfab648245160a44833d50a3d4686893d
anakon73 <nazarkinah73@gmail.com>
2023-12-08 17:17:25 +0200

    feat(SmsConfirmPage): prev and next input on arrows & prev on backspace

--- src/scripts/SMSConfirmForm.js
+++ src/scripts/SMSConfirmForm.js
@@ -1,6 +1,33 @@
-document.querySelector(".digits").addEventListener("input", function (e) {
-  e.target.value = e.data.replace(/[^0-9]/g, '');
-  if (e.target.value !== "" && e.target.nextElementSibling && e.target.nextElementSibling.nodeName === "INPUT") {
-    e.target.nextElementSibling.focus();
+// document.querySelector(".digits").addEventListener("input", function (e) {
+//   e.target.value = e.data.replace(/[^0-9]/g, "")
+//   if (
+//     e.target.value !== "" &&
+//     e.target.nextElementSibling &&
+//     e.target.nextElementSibling.nodeName === "INPUT"
+//   ) {
+//     e.target.nextElementSibling.focus()
+//   }
+// })
+
+document.querySelector(".digits").addEventListener("keydown", (event) => {
+  if (event.code === "Backspace") {
+    event.target.value = ""
+    event.preventDefault()
+    if (event.target.previousElementSibling) {
+      event.target.previousElementSibling.focus()
+    }
+  } else if (event.code === "ArrowRight") {
+    event.target.nextElementSibling.focus()
+  } else if (
+    event.code === "ArrowLeft" &&
+    event.target.previousElementSibling
+  ) {
+    event.target.previousElementSibling.focus()
+  } else if (
+    event.target.value !== "" &&
+    event.target.nextElementSibling &&
+    event.target.nextElementSibling.nodeName === "INPUT"
+  ) {
+    event.target.nextElementSibling.focus()
   }
-});
+})
