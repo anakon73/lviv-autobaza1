a8573d7baaaf25f0329d4684a9864b797265dadd
anakon73 <nazarkinah73@gmail.com>
2023-12-15 17:20:10 +0200

    feat: add open eye icon when password is showed

--- src/scripts/PasswordVisibility.js
+++ src/scripts/PasswordVisibility.js
@@ -1,9 +1,19 @@
-const passInput = document.getElementById('passwordInput')
+const passInput = document.getElementById("passwordInput")
+const openEyeImage = document.getElementById("openEyeImage")
+const closeEyeImage = document.getElementById("closeEyeImage")
 
-const showPassBtn = document.getElementById('showPass')
+const showPassBtn = document.getElementById("showPass")
 
 function changeVisibility() {
-  passInput.type = passInput.type === 'password' ? 'text' : 'password'
+  passInput.type = passInput.type === "password" ? "text" : "password"
+
+  if (openEyeImage.classList.contains("hidden")) {
+    openEyeImage.classList.remove("hidden")
+    closeEyeImage.classList.add("hidden")
+  } else {
+    openEyeImage.classList.add("hidden")
+    closeEyeImage.classList.remove("hidden")
+  }
 }
 
-showPassBtn.addEventListener('click', changeVisibility)
+showPassBtn.addEventListener("click", changeVisibility)
